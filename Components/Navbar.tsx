12e7f40ce9e8378596f8a965e9e189aba6687f3b
newton786241 <24tamseelanam11@gmail.com>
2025-02-01 20:35:58 +0530

    test_2

---
+++ Components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div id="navbar" className="py-10 px-20 hidden sm:flex text-white bg-black justify-around items-center">
+      <p>Home</p>
+      <p>About</p>
+      <p>Projects</p>
+      <p>Contact Us</p>
+      <p>Testimonials</p>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/Components/Navbar";
 
 export default function Home() {
   return (
-    <div id="navbar" className="py-10 px-20 hidden sm:flex text-white bg-black justify-around items-center">
-      <p>Home</p>
-      <p>About</p>
-      <p>Projects</p>
-      <p>Contact Us</p>
-      <p>Testimonials</p>
-    </div>
+    <section>
+      <Navbar/>
+    </section>
   );
 }
